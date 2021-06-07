0d9e44b820168af0978bb62eba8356c5c5b0edee
Jorge Morales <jormorveg@gmail.com>
2021-06-07 00:34:59 -0500

    q

--- examples/example_login.php
+++ examples/example_login.php
@@ -38,9 +38,9 @@ if(isset($_GET['code']) || isset($_SESSION['access_token'])) {
 		}
 	}
 
-	echo '<pre>';
+
 		print_r(json_encode($_SESSION));
-	echo '</pre>';
+
 
 } else {
 	echo '<a href="' . $meli->getAuthUrl($redirectURI, Meli::$AUTH_URL[$siteId]) . '">Login using MercadoLibre oAuth 2.0</a>';
